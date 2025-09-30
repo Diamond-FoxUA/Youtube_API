c5bf2a327bcc82a5629b1fe52f7998abd1430921
Dmytro Farbun <dimofarb@gmail.com>
2025-09-30 20:17:23 +0300

    add search bar + styles

---
+++ src/js/ux-ui.js
@@ -0,0 +1,9 @@
+// export function enableButton(btn, input) {
+//     btn.style.zIndex = '1';
+//     btn.style.backgroundColor = 'rgb(100, 0, 200)';
+// };
+
+// export function disableButton(btn, input) {
+//   btn.style.zIndex = '-1';
+//   btn.style.backgroundColor = 'rgb(85, 0, 170)';
+// }
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { enableButton, disableButton } from './js/ux-ui.js';
+
+const inputEl = document.querySelector('.search-input');
+const searchBtnEl = document.querySelector('.search-btn');
+
+
+
