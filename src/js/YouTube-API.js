55a0566c36d8dc2dbcec0ca40e2171f675d3284e
Dmytro Farbun <dimofarb@gmail.com>
2025-10-03 18:00:57 +0300

    add handleClick, data shows in the console

--- src/js/YouTube-API.js
+++ src/js/YouTube-API.js
@@ -3,20 +3,20 @@ import axios from 'axios';
 const API_KEY = "AIzaSyCDg8YNpb_-7aAum3KjZ3dvcD0ZALEWH48";
 const API_ADDRESS = "https://www.googleapis.com/youtube/v3/search";
 
-async function handleClick(query) {
+export async function getVideosByQuerry(query) {
   try {
-    const result = await axios(API_ADDRESS, {
+    const result = await axios.get(API_ADDRESS, {
       params: {
         q: query,
         key: API_KEY,
-        maxResult: 10,
+        maxResults: 10,
         type: 'video',
         part: 'snippet'
       }
     });
-    console.log(result);
+    console.log(result.data);
 
   } catch (error) {
     console.log(error);
   }
-}
+};
--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
-import { enableButton, disableButton } from './js/ux-ui.js';
+import { getVideosByQuerry } from './js/YouTube-API';
 
 const inputEl = document.querySelector('.search-input');
 const searchBtnEl = document.querySelector('.search-btn');
 
+let currentQuery = '';
+
+searchBtnEl.addEventListener('click', handleClick);
+
+async function handleClick(event) {
+  event.preventDefault();
+
+  currentQuery = inputEl.value.trim();
+
+  if (!currentQuery) {
+    alert('Empty querry!');
+    return;
+  }
+
+  const result = await getVideosByQuerry(currentQuery);
+}
 
 
 
